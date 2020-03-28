4e6397a97dbe1b6c2e43a8b844e783b627d51d62
Willyan Santos <willyan41@hotmail.com>
2020-03-27 21:06:36 -0300

    Adicionar arquivos de projeto.

---
+++ Cap04Fixacao/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap04Fixacao
+{
+    public class ConversorDeMoeda
+    {
+        public double Valor;
+        public double Quantidade;
+
+        public double ValorPago()
+        {
+            return (Valor * Quantidade)* 1.06;
+
+        }
+
+
+    }
+}
---
+++ Cap04Fixacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap04Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoeda cvm = new ConversorDeMoeda();
+
+            Console.Write("Qual a cotação do dólar? ");
+            cvm.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            cvm.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais {0}", cvm.ValorPago().ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
